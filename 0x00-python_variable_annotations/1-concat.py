673ee60569e259a4118da9270a1c57ce4dccd617
Becky1703 <oluwabukunmia@gmail.com>
2023-10-05 11:59:13 +0000

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Basic annotation to concat two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """function adds two strings and returns a string"""
+    return str1 + str2
