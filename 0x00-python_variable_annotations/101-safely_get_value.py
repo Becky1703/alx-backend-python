c490a30cf57bf8273914bcd8549a2d52fd4c7474
Becky1703 <oluwabukunmia@gmail.com>
2023-10-05 14:46:18 +0000

    corrected pycodestyle error

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -7,7 +7,10 @@ T = TypeVar('T')
 Res = Union[Any, T]
 Def = Union[T, None]
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None]) -> Union[Any, T]:
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None]
+                     ) -> Union[Any, T]:
+    """Type annotation"""
     if key in dct:
         return dct[key]
     else:
