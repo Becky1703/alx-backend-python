ba4cc517c7d62bc90a9334e0a59a9a7dad4ea704
Becky1703 <oluwabukunmia@gmail.com>
2023-10-05 11:47:55 +0000

    add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Basic annotation to add numbers """
+
+
+def add(a: float, b: float) -> float:
+    """function adds two float numbers and returns a float number"""
+    return a + b
