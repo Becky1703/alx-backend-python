24b5f6da81a15ba3a8d1a48e7ccc5345661807d1
Becky1703 <oluwabukunmia@gmail.com>
2023-10-05 12:18:55 +0000

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Basic annotation to floor a float number"""
+import math
+
+
+def floor(n: float) -> int:
+    """Function takes a float and returns the floor of the number"""
+    return int(n)
