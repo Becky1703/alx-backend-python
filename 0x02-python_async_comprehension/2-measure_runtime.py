a1c20cfaaef68e2134a757f8dd9af6492c48177f
Becky1703 <oluwabukunmia@gmail.com>
2023-10-10 12:24:03 +0000

    runtime for 4 parallel comprehension

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Asynchronous Comprehension"""
+import asyncio
+import time
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """ """
+    strt_time = time.time()
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+    return time.time() - strt_time
