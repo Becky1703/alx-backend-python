16b230c43ca39b1c72081d1d6513eb4341503339
Becky1703 <oluwabukunmia@gmail.com>
2023-10-10 12:10:39 +0000

    error fix

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,13 +2,14 @@
 """Asynchronous Comprehension"""
 import asyncio
 import random
-from typing import Generator
+from typing import List
+from importlib import import_module as using
 
 
-async_generator = __import__('0-async_generator').async_generator
+async_generator = using('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """function takes 10 random numbers using an async comprehension
     and returns 10 random numbers"""
     return [num async for num in async_generator()]
